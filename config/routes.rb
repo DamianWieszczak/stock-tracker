29bfc453eede6f2bab49fddd6b497708f5befe5d
DamianWieszczak <damian.wieszczak@gmail.com>
2022-05-10 17:32:20 +0200

    Add JS response to find friends

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'my_market', to: 'users#my_market'
   get 'find_stock', to: 'stocks#find'
   get 'friends', to: 'users#friends'
-  get 'find_friend', to: 'friends#find'
+  get 'find_friend', to: 'users#find'
 end
